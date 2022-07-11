1e551b5f0dd4842f062a7907c529d43c3c5f087d
RoR9 <valeriu.verebceanu@gmail.com>
2022-07-11 16:29:17 +0300

    Final version

--- script.js
+++ script.js
@@ -59,3 +59,50 @@ setTimeout(()=>{
 
 }
 
+
+
+const showAllBtn=document.querySelector(".show-all-btn")
+const hideElements=document.querySelectorAll("._hide")
+
+showAllBtn.addEventListener("click",function(e){
+     e.preventDefault()
+    hideElements.forEach(hideElement=>hideElement.classList.toggle("_hide"))
+    hideElements.forEach(hideElement=>hideElement.classList.toggle("_show"))
+    showAllBtn.classList.toggle("_show")
+    if(showAllBtn.classList.contains("_show")){
+        showAllBtn.innerHTML=`Hide all`
+        showAllBtn.href="#welcome-section"
+    }
+    else{
+        showAllBtn.innerHTML=`Show all<span class="icon">></span>`
+        showAllBtn.href=""
+
+    }
+   
+})
+
+//Menu Burger
+const menuBurger=document.querySelector(".header_burger")
+const navMenu=document.querySelector(".nav-bar-elements")
+const body=document.querySelector("body")
+const links=document.querySelectorAll(".nav-bar-elements")
+
+menuBurger.addEventListener("click",function(){
+    menuBurger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+    body.classList.toggle("lock")
+})
+
+links.forEach(link=>link.addEventListener("click",function(e){
+    
+    
+        if(e.target.parentElement.parentElement.classList.contains("active")){
+            menuBurger.classList.remove("active")
+            navMenu.classList.remove("active")
+            body.classList.remove("lock")
+        }
+    
+}))
+
+
+
